29c1a1d8d8fb8235885b92e27de21a64af0a9d2b
jarnold1899 <jarnold1899@gmail.com>
2016-01-10 16:45:59 +0100

    połączenie z bazą i stworzenie tablic

---
+++ ZPOChat/src/main/java/pl/us/edu/main/Main.java
@@ -0,0 +1,26 @@
+package pl.us.edu.main;
+
+import org.hibernate.annotations.common.util.impl.Log;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
+import pl.us.edu.model.Message;
+import pl.us.edu.model.User;
+
+@SuppressWarnings("deprecation")
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		AnnotationConfiguration config = new AnnotationConfiguration();
+//		config.addAnnotatedClass(User.class);
+//		config.addAnnotatedClass(Log.class);
+//		config.addAnnotatedClass(Message.class);
+//		config.configure("hibernate.cfg.xml");
+//		new SchemaExport(config).create(true, true);
+
+	}
+
+}
